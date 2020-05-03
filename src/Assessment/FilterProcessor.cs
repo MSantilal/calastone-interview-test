88b37abd4ba059692aa007b053a81948633da4cf
MSantilal <MSantilal@users.noreply.github.com>
2020-05-03 17:38:27 +0100

    Assement up to time limit of 2hrs

---
+++ src/Assessment/FilterProcessor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Assessment.Filters;
+
+namespace Assessment
+{
+    internal sealed class FilterProcessor
+    {
+        private readonly IEnumerable<IFilter> _filters;
+        private readonly StringBuilder _builder;
+
+        /// <summary>
+        /// Initialises an instance of <see cref="FilterProcessor"/>
+        /// </summary>
+        /// <param name="filters"></param>
+        public FilterProcessor(IEnumerable<IFilter> filters)
+        {
+            _filters = filters;
+            _builder = new StringBuilder();
+        }
+
+        /// <summary>
+        /// Returns a string of words processed by a filter
+        /// </summary>
+        /// <param name="input">Word to filter</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="input"/> is <see langword="null" />
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="input"/> consists only of white space.
+        /// </exception>
+        public string Process(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentNullException(input, $"Param: {nameof(input)} is null.");
+            }
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentOutOfRangeException(input, $"Param: {nameof(input)} is null or has white space characters.");
+            }
+
+            foreach (var item in _filters)
+            {
+                if (item.Filter(input))
+                {
+                    _builder.AppendLine(input);
+                }
+            }
+
+            return _builder.ToString();
+        }
+
+    }
+}
--- src/Assessment/FilterProgram.cs
+++ src/Assessment/FilterProgram.cs
@@ -1,10 +1,14 @@
-﻿namespace Assessment
+﻿using System.IO;
+
+namespace Assessment
 {
     internal sealed class FilterProgram
     {
         public FilterProgram()
         {
-            
+            var text = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "assessment-text.txt"));
+
+            var splitText = text.Split(' ');
         }
     }
 
